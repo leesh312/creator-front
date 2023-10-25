a75aca9ccaf0b60aab1780d6d2f39d1fd7a8ba3a
leesh <leesh312@gmail.com>
2023-10-25 23:23:14 +0900

    feature: typescript, eui 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
+import {EuiText} from "@elastic/eui";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
+      <EuiText><p>Hello World!</p></EuiText>
     </div>
   );
 }
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
+import {EuiProvider} from '@elastic/eui';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <EuiProvider colorMode="dark">
+      <App />
+    </EuiProvider>
+  </React.StrictMode>
+);
